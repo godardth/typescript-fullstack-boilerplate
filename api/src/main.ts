89f91e4826302d2ea046bb8320457e8cb107e96e
Theo Godard <theo.godard@gmail.com>
2024-03-11 00:32:17 +0900

    Configure the Swagger documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,20 @@
 import "reflect-metadata";
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('Documentation automatically generated')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
