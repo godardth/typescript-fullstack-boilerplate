833a9b4d6cc24622c3b67c999989d3c1bcd3bce3
Theo Godard <theo.godard@gmail.com>
2024-03-17 17:44:08 +0900

    Add a server deploy script

---
+++ prod/server-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+apt install docker-ce
+systemctl status docker
+docker compose up
